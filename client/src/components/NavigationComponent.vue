f25199730211300c9fe6b6f4862ca4570fbf9ffd
Vojin <puricvojin@gmail.com>
2025-01-23 22:30:57 +0100

    refactored code and converted to PrimeVue UI

---
+++ client/src/components/NavigationComponent.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import Menubar from "primevue/menubar"
+
+const router = useRouter()
+const navigate = (whereTo: string | undefined) => {
+  if (whereTo) {
+    router.push(whereTo)
+  }
+}
+const items = ref([
+  {
+    label: 'Home',
+    url: '/',
+  },
+  {
+    label: 'Crafting List',
+    url: '/buildList',
+  },
+  {
+    label: 'Data',
+    items: [
+      {
+        label: 'Designs',
+        url: '/designs',
+      },
+      {
+        label: 'Materials',
+        url: '/materials',
+      },
+      {
+        separator: true
+      },
+      {
+        label: 'Weapons',
+        url: '/weapons',
+      },
+      {
+        label: 'Grindstone',
+        url: '/grindstone',
+      },
+      {
+        label: 'Enemies',
+        url: '/enemies',
+      },
+    ]
+  }]);
+</script>
+<template>
+  <div class="card">
+    <Menubar :model="items">
+      <template #start>
+      </template>
+      <template #item="{ item, props, hasSubmenu, root }">
+        <a v-ripple class="flex align-items-center" v-bind="props.action" @click="navigate(item.url)">
+          <span class="ml-2">{{ item.label }}</span>
+          <i v-if="hasSubmenu"
+            :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"></i>
+        </a>
+      </template>
+    </Menubar>
+  </div>
+</template>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,24 @@
-import './assets/main.css'
+import PrimeVue from "primevue/config"
+import "primevue/resources/primevue.min.css"
+import "primevue/resources/themes/aura-dark-purple/theme.css"
+import "../node_modules/primeflex/primeflex.css"
+
+import Ripple from "primevue/ripple"
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Button from "primevue/button"
 
-const app = createApp(App)
+
+const app = createApp(App);
 
 app.use(router)
+app.use(PrimeVue);
+
+//PrimeVue reused stuff //TODO extract later
+app.component('Button', Button);
+app.directive("ripple", Ripple)
+
 
 app.mount('#app')
