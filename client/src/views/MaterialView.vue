404bd4aaf31dc9eca42100c3c54f7466ce8c596b
Vojin Puric <puricvojin@gmail.com>
2025-01-29 21:37:13 +0100

    Fixed material view

--- client/src/views/MaterialView.vue
+++ client/src/views/MaterialView.vue
@@ -2,42 +2,46 @@
 import { ref, onMounted } from "vue"
 import { useRoute } from "vue-router"
 const route = useRoute()
-const material = ref([])
+const material = ref(null)
 const trades = ref([])
+
 onMounted(() => {
   const id = route.params.id
-  console.log(id)
-  fetch(`http://localhost:3000/requirement/${id}`)
+  fetch(`http://localhost:3000/item/${id}`)
     .then(d => d.json().then(data => {
-      console.log(data)
       material.value = data
     }))
     .catch(e => console.error(e))
 
   fetch(`http://localhost:3000/trades/${id}`)
     .then(d => d.json().then(data => {
-      console.log(data)
       trades.value = data
     }))
     .catch(e => console.error(e))
-
 })
+
 </script>
 <template>
-  <div>
+  <div v-if="material != null">
     <h2>{{ material.name }}</h2>
     <Card>
-      <h3>Used For:</h3>
-      <div v-for="(f, i) in material" :key=i>
-        <router-link :to="`/design/${f.id}`">{{ f.name }}</router-link>
-        : {{ f.quantity }}
-      </div>
+      <template #content>
+        <h3>Used For:</h3>
+        <p class="m-0">
+        <div v-for="(f, i) in material.designs" :key=i>
+          <router-link :to="`/design/${f.designId}`">{{ f.design }}</router-link>
+          : {{ f.quantity }}
+        </div>
+        </p>
+      </template>
     </Card>
     <Card>
-      <h3>Trades</h3>
-      <div v-for="(f, i) in trades" :key=i>
-        {{ f.giveName }} X {{ f.giveQuantity }} -> {{ f.getName }} X {{ f.getQuantity }}
-      </div>
+      <template #content>
+        <h3>Trades</h3>
+        <div v-for="(f, i) in trades" :key=i>
+          {{ f.giveName }} X {{ f.giveQuantity }} -> {{ f.getName }} X {{ f.getQuantity }}
+        </div>
+      </template>
     </Card>
   </div>
 </template>
