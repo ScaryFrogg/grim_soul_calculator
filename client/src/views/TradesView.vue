a1f6ee4293dfb347827b820a6589027d20c53ba8
Vojin <puricvojin@gmail.com>
2025-02-02 20:21:36 +0100

    added filtering on trades view

--- client/src/views/TradesView.vue
+++ client/src/views/TradesView.vue
@@ -2,6 +2,13 @@
 import { ref, onMounted } from "vue"
 import { useRouter } from "vue-router"
 import type { Trade } from "@/types"
+import DataTable from "primevue/datatable"
+import Column from "primevue/column"
+import InputText from "primevue/inputtext"
+import { FilterMatchMode } from "primevue/api";
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
 const router = useRouter()
 const trades = ref<Trade[]>([])
 
@@ -21,14 +28,31 @@ const navigate = (whereTo: number | undefined) => {
 </script>
 <template>
   <h2>Trades</h2>
-  <Card>
-    <template #content>
-      <div v-for="(f, i) in trades" :key=i>
-        <span @click="navigate(f.giveId)">{{ f.giveName }}</span>
-        X {{ f.giveQuantity }} ->
-        <span @click="navigate(f.getId)">{{ f.getName }}</span>
-        X {{ f.getQuantity }}
-      </div>
-    </template>
-  </Card>
+  <div class="flex justify-content-center">
+    <DataTable :value="trades" tableStyle="max-width: 100vw; width: 50rem" removableSort :filters="filters"
+      :globalFilterFields="['giveName', 'getName']" stripedRows>
+      <template #header>
+        <div class="flex justify-content-center">
+          <InputText v-model="filters['global'].value" placeholder="Search" />
+        </div>
+      </template>
+      <Column field="give" header="Give" sortField="giveName" sortable bodyClass="px-0 md:px-5">
+        <template #body="{ data }">
+          <span @click="navigate(data.giveId)">{{ data.giveName }}</span>
+          X {{ data.giveQuantity }}
+        </template>
+      </Column>
+      <Column field="split" bodyClass="p-0">
+        <template #body>
+          ->
+        </template>
+      </Column>
+      <Column field="get" header="Get" sortField="getName" sortable bodyClass="px-0 md:px-5">
+        <template #body="{ data }">
+          <span @click="navigate(data.getId)">{{ data.getName }}</span>
+          X {{ data.getQuantity }}
+        </template>
+      </Column>
+    </DataTable>
+  </div>
 </template>
--- client/src/views/WeaponsView.vue
+++ client/src/views/WeaponsView.vue
@@ -21,22 +21,22 @@ onMounted(() => {
 
 </script>
 <template>
-  <div class="card">
-    <DataTable :filters="filters" :value="weapons" tableStyle="min-width: 50rem"
-      :globalFilterFields="['name', 'damage']">
+  <div class="flex justify-content-center" style="max-width: 100vw;">
+    <DataTable size="small" :filters="filters" :value="weapons" tableStyle="max-width: 100vw; width: 50rem"
+      :globalFilterFields="['name', 'damage']" removableSort>
       <template #header>
         <div class="flex justify-content-center">
           <InputText v-model="filters['global'].value" placeholder="Search" />
         </div>
       </template>
-      <Column field="name" header="Weapon" sortable style="width: 23%"></Column>
-      <Column field="damage" header="Dmg" sortable style="width: 11%"></Column>
-      <Column field="attackSpeed" header="AS" sortable style="width: 11%"></Column>
-      <Column field="s1" header="s+1" sortable style="width: 11%"></Column>
-      <Column field="s2" header="s+2" sortable style="width: 11%"></Column>
-      <Column field="s3" header="s+3" sortable style="width: 11%"></Column>
-      <Column field="s4" header="s+4" sortable style="width: 11%"></Column>
-      <Column field="s5" header="s+5" style="width: 11%"></Column>
+      <Column field="name" header="Weapon" sortable headerClass="px-1 md:px-5" bodyClass="px-0 md:px-5"></Column>
+      <Column field="damage" header="Dmg" sortable headerClass="px-1 md:px-5" bodyClass="px-0 md:px-5"></Column>
+      <Column field="attackSpeed" header="AS" sortable headerClass="px-1 md:px-5" bodyClass="px-0 md:px-5"></Column>
+      <Column field="s1" header="s+1" sortable headerClass="px-1 md:px-5" bodyClass="px-0 md:px-5"></Column>
+      <Column field="s2" header="s+2" sortable headerClass="px-1 md:px-5" bodyClass="px-0 md:px-5"></Column>
+      <Column field="s3" header="s+3" sortable headerClass="px-1 md:px-5" bodyClass="px-0 md:px-5"></Column>
+      <Column field="s4" header="s+4" sortable headerClass="px-1 md:px-5" bodyClass="px-0 md:px-5"></Column>
+      <Column field="s5" header="s+5" sortable headerClass="px-1 md:px-5" bodyClass="px-0 md:px-5"></Column>
     </DataTable>
   </div>
 </template>
